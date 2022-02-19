a38e9e58aa7ac3462871b65aa8ba9502d827aecd
Ross Hardingham <30514204+RossJH@users.noreply.github.com>
2022-02-19 12:16:29 +1100

    Added h1 & p text

---
+++ week-1/dog-years/main.js
@@ -0,0 +1,32 @@
+
+
+// 1. My Age as Dog years. 
+let myAge = 30;
+
+// 2. The early years comment. 
+let earlyYears = 2;
+
+earlyYears *= 10.5;
+console.log(earlyYears);
+
+// New variable that can be changed later. 
+var laterYears = myAge - 2;
+
+
+// work out later dog years to my age. 
+laterYears *= 4;
+console.log(laterYears);
+
+// Final variable that shows my age in dog years. 
+var myAgeInDogYears = earlyYears + laterYears;
+console.log(earlyYears + laterYears);
+console.log(myAgeInDogYears);
+
+
+var myName = 'Ross'
+console.log(myName.toLowerCase());
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
+
+
+
