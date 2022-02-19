7f93935d83472364e6cb3454fd28be1015f68f16
Ross Hardingham <30514204+RossJH@users.noreply.github.com>
2022-02-19 12:35:59 +1100

    Completed additional practice.

--- week-1/dog-years/main.js
+++ week-1/dog-years/main.js
@@ -1,5 +1,7 @@
 
 
+// DOG YEARS PROJECT //
+
 // 1. My Age as Dog years. 
 let myAge = 30;
 
@@ -28,5 +30,34 @@ console.log(myName.toLowerCase());
 
 console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
 
+// -------------------------------------------------------//
+// Additional practice 
+
+// 1. My Age as Dog years. 
+let myAge1 = 30;
+
+// 2. The early years comment. 
+let earlyYears1 = 2;
+
+var myEarlYears1 = earlyYears1 * 10.5;
+var myAgeDogYears = myEarlYears1;
+console.log(myAgeDogYears)
+
+// New variable that can be changed later. 
+var laterYears1 = myAge - 2;
+
+// work out later dog years to my age. 
+var myLaterYears1 = laterYears1 * 4;
+var myAgeDogYears1 = myLaterYears1;
+console.log(myAgeDogYears1);
+
+// Final variable that shows my age in dog years. 
+var myAgeInTotalDogYears = myAgeDogYears + myAgeDogYears1;
+console.log(myAgeDogYears + myAgeDogYears1);
+console.log(myAgeInTotalDogYears);
 
 
+var myName1 = 'Ross'
+console.log(myName1.toLowerCase());
+
+console.log(`My name is ${myName1}. I am ${myAge1} years old in human years which is ${myAgeInTotalDogYears} years old in dog years.`)
