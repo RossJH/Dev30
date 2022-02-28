082cb35a6db42aab86ed74280427a25225eec10b
Ross Hardingham <30514204+RossJH@users.noreply.github.com>
2022-02-28 11:02:20 +1100

    RPS game update

---
+++ week-2/rock-paper-scissors/main.js
@@ -0,0 +1,74 @@
+
+var humanScore = 0;
+var computerScore = 0;
+
+
+//User options Btn's clicks assigns to User choices. 
+document.getElementById('rock').onclick = playRock;
+document.getElementById('paper').onclick = playPaper;
+document.getElementById('scissors').onclick = playScissors;
+
+//User choices
+function playRock() {
+  getUserChoice("rock");
+}
+function playPaper() {
+  getUserChoice("paper");
+}
+function playScissors() {
+  getUserChoice("scissors");
+}
+
+function getUserChoice(userChoice) {
+  
+  computerChoice = getComputerChoice();
+
+  //Displays User and Bot choices. 
+  document.getElementById('status').innerHTML = "<p>You played <strong>" + userChoice + "</strong>. The bot played <strong>" + computerChoice + "</strong>.</p>";
+  
+
+  //Compares user choice against computer choice to provide outcomes & tally the score. 
+  if(userChoice == 'rock') {
+    if(computerChoice == 'rock') {
+      document.getElementById('status').innerHTML += "<p>You tied. :|</p>";
+    } else if (computerChoice == 'paper') {
+      document.getElementById('status').innerHTML += "<p>You lose. :(</p>";
+      computerScore++;
+    } else if (computerChoice == 'scissors') {
+      document.getElementById('status').innerHTML += "<p>You win! :)</p>";
+      humanScore++;
+    }
+  } else if (userChoice == 'paper') {
+    if(computerChoice == 'rock') {
+      document.getElementById('status').innerHTML += "<p>You win! :)</p>";
+      humanScore++;
+    } else if (computerChoice == 'paper') {
+      document.getElementById('status').innerHTML += "<p>You tied. :|</p>";
+    } else if (computerChoice == 'scissors') {
+      document.getElementById('status').innerHTML += "<p>You lose. :(</p>";
+      computerScore++;
+    }  
+  } else if (userChoice == 'scissors') {
+    if(computerChoice == 'rock') {
+      document.getElementById('status').innerHTML += "<p>You lose. :(</p>";
+      computerScore++;
+    } else if (computerChoice == 'paper') {
+      document.getElementById('status').innerHTML += "<p>You win! :)</p>";
+      humanScore++;
+    } else if (computerChoice == 'scissors') {
+      document.getElementById('status').innerHTML += "<p>You tied. :|</p>";
+    }  
+  }
+  
+  //Assigns values to the scoreboard. 
+  document.getElementById('humanScore').innerHTML = humanScore;
+  document.getElementById('computerScore').innerHTML = computerScore;
+  
+}
+
+// Gets computer choice randomly and assigns it to the 3 options. 
+function getComputerChoice() {
+  var plays = ['rock', 'paper', 'scissors'];
+  var play = plays[Math.floor(Math.random() * plays.length)];
+  return play;
+}
