281e7179bc599ea9a40966c3b4c09e940be398b5
Ross Hardingham <30514204+RossJH@users.noreply.github.com>
2022-03-06 13:47:55 +1100

    Week 3 update

---
+++ week-3/secret-msg/main.js
@@ -0,0 +1,19 @@
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 
+'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+
+secretMessage.pop ()
+
+
+secretMessage.push('to', 'program')
+
+secretMessage.splice(7, 1, 'right')
+
+secretMessage.shift ()
+
+secretMessage.unshift( 'Programming')
+
+const start = 6;
+const deleteCount = 5;
+const removedItems = secretMessage.splice(start, deleteCount, 'know');
+
+console.log(secretMessage.join( ));
